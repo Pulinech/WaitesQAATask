c6d5fd6e245824d7a947da642d600dde7ce9842c
Pulinech <pulinech1@gmail.com>
2021-10-01 15:38:38 +0300

    Remade delays to test_data parameter and improved todo_page methods for correct assert, based on empty/not empty parameter

--- pages/todo_page.py
+++ pages/todo_page.py
@@ -1,15 +1,18 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 class TodoPage(BasePage):
-    def prepare_test_data(self, task_list, mark_tasks=[]):
+    def prepare_test_data(self, task_list, mark_tasks=[], delay=0):
         self.task_list = task_list
         self.mark_tasks = mark_tasks
         self.active_tasks = [i for i in task_list if i not in mark_tasks]
+        self.delay = delay
 
     def create_tasks(self):
+        time.sleep(self.delay)
         task_input = self.is_element_present(By.CSS_SELECTOR, ".new-todo")
         assert task_input, "Can't find input for tasks"
         task_input.click()
@@ -17,32 +20,50 @@ class TodoPage(BasePage):
         for i in range(tl_len):
             task_input.send_keys(self.task_list[i])
             task_input.send_keys(Keys.RETURN)
-
-    def should_be_created_tasks(self, task_list=[]):
-        tl_len = len(task_list)
-        for i in range(tl_len):
-            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{task_list[i]}']"), \
-                f"Task {task_list[i]} should be created in todo list"
+        time.sleep(self.delay)
 
     def mark_next_tasks(self):
         mt_len = len(self.mark_tasks)
         for i in range(mt_len):
             task = self.is_element_present(By.XPATH,
                                            f"//*[label='{self.mark_tasks[i]}']/input[contains(@class,'toggle')]")
+            assert task, f"There is no task '{self.mark_tasks[i]}' to mark it"
             task.click()
+        time.sleep(self.delay)
+
+    def should_be_created_tasks(self, task_list=[]):
+        if task_list:
+            list = task_list
+        else:
+            list = self.task_list
+        tl_len = len(list)
+        for i in range(tl_len):
+            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{list[i]}']"), \
+                f"Task '{list[i]}' should be created in todo list"
+        time.sleep(self.delay)
 
     def should_be_active_tasks(self, active_tasks=[]):
+        if active_tasks:
+            list = active_tasks
+        else:
+            list = self.active_tasks
         active_link = self.browser.find_element_by_link_text("Active")
         active_link.click()
-        at_len = len(active_tasks)
+        at_len = len(list)
         for i in range(at_len):
-            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{active_tasks[i]}']"),\
-                f"Task {active_tasks[i]} should be active"
+            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{list[i]}']"),\
+                f"Task '{list[i]}' should be active"
+        time.sleep(self.delay)
 
     def should_be_completed_tasks(self, mark_tasks=[]):
+        if mark_tasks:
+            list = mark_tasks
+        else:
+            list = self.mark_tasks
         complete_link = self.browser.find_element_by_link_text("Completed")
         complete_link.click()
-        ct_len = len(mark_tasks)
+        ct_len = len(list)
         for i in range(ct_len):
-            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{mark_tasks[i]}']"),\
-                f"Task {mark_tasks[i]} should be completed"
+            assert self.is_element_present(By.CSS_SELECTOR, f".edit[value='{list[i]}']"),\
+                f"Task '{list[i]}' should be completed"
+        time.sleep(self.delay)
--- test_todo_page.py
+++ test_todo_page.py
@@ -6,20 +6,11 @@ def test_add_tasks_mark_them_and_check_marks(browser):
     link = "https://todomvc.com/examples/react/#/"
     pg = TodoPage(browser, link)
     pg.open()
-    pg.prepare_test_data(["a", "b", "c"], ["b"])
     # 2 seconds delays for visual review
-    time.sleep(2)
-
+    pg.prepare_test_data(["a", "b", "c"], ["b"], 2)
     pg.create_tasks()
-    # should_*() methods created for testing, leave it empty or add some list values for test check
+    # should_be_*() methods created for testing, leave it empty or add some list values for test failure check
     pg.should_be_created_tasks()
-    time.sleep(2)
-
     pg.mark_next_tasks()
-    time.sleep(2)
-
     pg.should_be_active_tasks()
-    time.sleep(2)
-
     pg.should_be_completed_tasks()
-    time.sleep(2)
